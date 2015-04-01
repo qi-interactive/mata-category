e0a738121f3e4cf49feec30c85bfe58e31f8ad20
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2015-04-01 18:30:26 +0100

    Updates
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

--- behaviors/CategoryActiveFormBehavior.php
+++ behaviors/CategoryActiveFormBehavior.php
@@ -11,8 +11,15 @@ use yii\helpers\ArrayHelper;
 class CategoryActiveFormBehavior extends  \yii\base\Behavior {
 
 	public function category($options = []) {
+		if(isset($this->owner->options['class'])) {
+		    $this->owner->options['class'] .= ' multi-choice-dropdown half-max-width-item';
+		} else {
+			$this->owner->options['class'] = ' multi-choice-dropdown half-max-width-item';
+		}
+
 
 		$options = array_merge($this->owner->inputOptions, $options);
+
 
 		$this->owner->adjustLabelFor($options);
 		$this->owner->labelOptions["label"] = "Category"; 
